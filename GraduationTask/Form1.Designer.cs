3f117a66a4b4d616cdf0c26c458436c05877b339
Nikola Milun <nikola.milun2508@gmail.com>
2022-04-20 13:16:12 +0200

    Added the current progress

---
+++ GraduationTask/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿
+namespace GraduationTask
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.Šifra = new System.Windows.Forms.ColumnHeader();
+            this.Selo = new System.Windows.Forms.ColumnHeader();
+            this.Grad = new System.Windows.Forms.ColumnHeader();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbSifra = new System.Windows.Forms.TextBox();
+            this.tbNaziv = new System.Windows.Forms.TextBox();
+            this.cbGrad = new System.Windows.Forms.ComboBox();
+            this.btnUnesiIzmene = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Šifra,
+            this.Selo,
+            this.Grad});
+            this.listView1.Font = new System.Drawing.Font("Segoe UI", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(283, 55);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(384, 352);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // Šifra
+            // 
+            this.Šifra.Text = "Šifra";
+            // 
+            // Selo
+            // 
+            this.Selo.Text = "Selo";
+            this.Selo.Width = 140;
+            // 
+            // Grad
+            // 
+            this.Grad.Text = "Grad";
+            this.Grad.Width = 140;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(22, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Šifra";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(22, 119);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Naziv";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(22, 173);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 25);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Grad";
+            // 
+            // tbSifra
+            // 
+            this.tbSifra.Font = new System.Drawing.Font("Segoe UI", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.tbSifra.Location = new System.Drawing.Point(110, 52);
+            this.tbSifra.Name = "tbSifra";
+            this.tbSifra.Size = new System.Drawing.Size(100, 31);
+            this.tbSifra.TabIndex = 4;
+            this.tbSifra.TextChanged += new System.EventHandler(this.tbSifra_TextChanged);
+            // 
+            // tbNaziv
+            // 
+            this.tbNaziv.Font = new System.Drawing.Font("Segoe UI", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.tbNaziv.Location = new System.Drawing.Point(110, 116);
+            this.tbNaziv.Name = "tbNaziv";
+            this.tbNaziv.Size = new System.Drawing.Size(143, 31);
+            this.tbNaziv.TabIndex = 5;
+            // 
+            // cbGrad
+            // 
+            this.cbGrad.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbGrad.Font = new System.Drawing.Font("Segoe UI", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.cbGrad.FormattingEnabled = true;
+            this.cbGrad.Location = new System.Drawing.Point(110, 170);
+            this.cbGrad.Name = "cbGrad";
+            this.cbGrad.Size = new System.Drawing.Size(143, 31);
+            this.cbGrad.TabIndex = 6;
+            // 
+            // btnUnesiIzmene
+            // 
+            this.btnUnesiIzmene.Font = new System.Drawing.Font("Segoe UI", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnUnesiIzmene.Location = new System.Drawing.Point(35, 307);
+            this.btnUnesiIzmene.Name = "btnUnesiIzmene";
+            this.btnUnesiIzmene.Size = new System.Drawing.Size(199, 45);
+            this.btnUnesiIzmene.TabIndex = 7;
+            this.btnUnesiIzmene.Text = "Unesi izmene >>>";
+            this.btnUnesiIzmene.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnUnesiIzmene);
+            this.Controls.Add(this.cbGrad);
+            this.Controls.Add(this.tbNaziv);
+            this.Controls.Add(this.tbSifra);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader Šifra;
+        private System.Windows.Forms.ColumnHeader Selo;
+        private System.Windows.Forms.ColumnHeader Grad;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbSifra;
+        private System.Windows.Forms.TextBox tbNaziv;
+        private System.Windows.Forms.ComboBox cbGrad;
+        private System.Windows.Forms.Button btnUnesiIzmene;
+    }
+}
+
---
+++ GraduationTask/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data.Common;
+
+namespace GraduationTask
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            using (SqlConnection cnn = new SqlConnection(@"Data Source= LIMUNPC\NIKOLASRVSQL; Initial Catalog= graduationTask; Integrated Security= true"))
+            {
+                cnn.Open();
+                SqlCommand sela = new SqlCommand("seloPodaci", cnn);
+                sela.CommandType = CommandType.StoredProcedure;
+                using (SqlDataReader sdr = sela.ExecuteReader())
+                {
+                    while (sdr.Read())
+                    {
+                        ListViewItem lwi = new ListViewItem(sdr.GetValue(0).ToString());
+                        lwi.SubItems.Add(sdr.GetValue(1).ToString().Trim(' '));
+                        lwi.SubItems.Add(sdr.GetValue(2).ToString().Trim(' '));
+                        listView1.Items.Add(lwi);
+                    }
+                }
+                SqlCommand gradovi = new SqlCommand("SELECT Grad FROM grad ORDER BY Grad ASC", cnn);
+                using (SqlDataReader sdr = gradovi.ExecuteReader())
+                {
+                    while (sdr.Read())
+                    {
+                        cbGrad.Items.Add(sdr.GetValue(0).ToString().Trim(' '));
+                    }
+                }
+            }
+        }
+
+        private void tbSifra_TextChanged(object sender, EventArgs e)
+        {
+            foreach (ListViewItem item in listView1.Items)
+            {
+                if (item.SubItems[0].Text == tbSifra.Text)
+                {
+                    MessageBox.Show("ide");
+                    item.Selected = true;
+                    cbGrad.SelectedItem = item.SubItems[2].Text.ToString();
+                }
+            }
+        }
+    }
+}
